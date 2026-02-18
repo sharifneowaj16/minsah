0f6d46a53565258817ef608be7cb6aaf1d6594d8
sharifneowaj16 <sharifneowaja16@gmail.com>
2026-02-18 03:40:28 -0800

    Update page.tsx

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-export const dynamic = "force-dynamic";
 
 import { useState, useEffect, useRef, useCallback } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
